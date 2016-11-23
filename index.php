15b07086fbb97a0dac0ed404855e7b6d43d66cf4
TUIL Alexandre <tuil_alexandre@hotmail.com>
2016-11-23 14:01:25 +0100

    exo finish

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exo php boucles</title>
+  </head>
+  <body>
+      <?php
+      //Exo 1
+          $i = 0;
+          while($i <= 10){
+            echo 'On compte jusqu\'à 10 avec la variable $i '.$i.'!</br>';
+            $i++;
+          }
+
+      //Exo 2
+          $var1 = 0;
+          $var2 = 50;
+          while($var1 <= 20){
+            $var1 * $m;
+            echo ''.$var1.'';
+            $var1++;
+          }
+
+echo '</br>';
+      //Exo 3
+          $i = 100;
+          $j = 2;
+          while($i >= 10){
+            $i * $j;
+            echo ''.$i.'';
+            $i--;
+          }
+
+echo '</br>';
+      //Exo 4
+          $i = 1;
+          while($i <=10){
+            $i / 2;
+            echo ''.$i.'';
+            $i++;
+          }
+
+echo '</br>';
+      //Exo 5
+          $i = 1;
+          while($i <= 15){
+            echo 'On y arrive presque.';
+            $i++;
+          }
+
+echo '</br>';
+      //Exo 6
+          $i = 20;
+          while($i >= 0){
+            echo 'C\'est presque bon.';
+            $i--;
+          }
+
+echo '</br>';
+      //Exo 7
+          $i = 1;
+          while($i <=100){
+            echo 'On tient le bon bout.'.$i.'</br>';
+            $i+=15;
+          }
+
+echo '</br>';
+      //Exo 8
+          $i = 200;
+          while($i > 0){
+            echo 'Enfin!!! '.$i.'</br>';
+            $i-=12;
+          }
+      ?>
+
+  </body>
+</html>
